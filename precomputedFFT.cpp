8a78b12e17afedd771b2d88b4d5c7067e3c60aec
DJKIM613 <best_sheild97@naver.com>
2020-04-30 18:10:03 +0900

    precomputedFFT

---
+++ precomputedFFT.cpp
@@ -0,0 +1,114 @@
+#include "precomputedFFT.h"
+#include <math.h>
+
+#include <iostream>
+using namespace std;
+
+void precompute_sincos() {
+    for(int i = 1 ; i <= SUBCARRIER_NUM ; i++) {
+        for(int j = 0 ; j < SUBCARRIER_NUM ; j++) {
+            float angle = 2 * M_PI / i * j;
+            precomputed_sin[i][j] = cos(angle);
+            precomputed_cos[i][j] = sin(angle);
+        }
+    }
+}
+
+void precompute_twiddle_factor() {
+    for(int i = 1 ; i <= SUBCARRIER_NUM ; i++){
+        for(int j = 0 ; j < SUBCARRIER_NUM ; j++) {
+            twiddle_factor[i][j] = Complex(precomputed_sin[i][j], precomputed_cos[i][j]);
+        }
+    }
+}
+
+void precompute_step_number() {
+    int radix_num = sizeof(radices) / sizeof(radices[0]);
+    for(int i = 1 ; i <= SUBCARRIER_NUM ; i++) {
+        int ii = i;
+        for(int j = 0 ; j < radix_num ; j++){
+            while(ii % radices[j] == 0) {
+                ii /= radices[j];
+                step_radix[i][step_number[i]] = radices[j];
+                step_number[i]++;
+            }
+        }
+    }
+}
+
+void precompute_permutation() {
+    for(int i = 1 ; i <= SUBCARRIER_NUM ; i++) {
+        for(int j = 0 ; j < SUBCARRIER_NUM ; j++) {
+            int ii = i, jj = j;
+            int radix_inverse = 0;
+            for(int k = 0 ; k < step_number[i] ; k++) {
+                radix_inverse += (jj % step_radix[i][k]) * (ii / step_radix[i][k]);
+                jj /= step_radix[i][k];
+                ii /= step_radix[i][k];
+            }
+            precomputed_permutation[i][j] = radix_inverse;
+        }
+    }
+}
+
+void precompute_all() {
+    precompute_sincos();
+    precompute_twiddle_factor();
+    precompute_step_number();
+    precompute_permutation();
+}
+
+void precomputed_fft(int size, Complex *list) {
+    Complex tmp_list[size];
+    for(int i = 0 ; i < size ; i++) tmp_list[i] = list[i];
+    for(int i = 0 ; i < size ; i++) list[precomputed_permutation[size][i]] = tmp_list[i];
+
+    int step_size = 1, previous_size = 1;
+    for(int i = step_number[size] - 1 ; i >= 0 ; i--){
+        int radix = step_radix[size][i];
+        previous_size = step_size;
+        step_size *= radix;
+        
+        debug(previous_size, step_size);
+
+        for(int j = 0 ; j < size ; j++) {
+            tmp_list[j] = list[j];
+            list[j].real = 0;
+            list[j].image = 0;
+        }
+
+        for(int j = 0 ; j < size ; j++) {
+            int base = j / step_size * step_size;
+            int offset = j % previous_size;
+            for(int k = 0 ; k < radix ; k++) {
+                list[j] += tmp_list[base + offset + k * previous_size] * twiddle_factor[step_size][k * previous_size];
+            }
+        }
+
+        for(int i = 0 ; i < size ; i++) cout << list[i] << ' ';
+        cout << '\n';
+    }
+}
+
+int sizes[] = {8};
+
+
+int main() {
+    Complex list[SUBCARRIER_NUM];
+
+    precompute_all();
+
+    for(int size : sizes) {
+        for(int i = 0 ; i < size ; i++) cout << twiddle_factor[size][i] << ' ';
+        cout << '\n';
+
+        for(int i = 1 ; i < size ; i++) list[i].real = 1;
+        list[0].real = 2;
+        
+        precomputed_fft(size, list);
+
+        for(int i = 0 ; i < size; i++) cout << list[i] << '\n';
+    }
+
+    return 0;
+}
---
+++ precomputedFFT.h
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+class Complex {
+    public :
+        float real, image;
+
+    public :
+    Complex(float real = 0, float image = 0) : real(real), image(image) {}
+    Complex operator + (const Complex &o) {
+        return Complex(this->real + o.real, this->image + o.image);
+    }
+    Complex operator - (const Complex &o) {
+        return Complex(this->real - o.real, this->image - o.image);
+    }
+    Complex operator * (const Complex &o) {
+        return Complex(this->real * o.real - this->image * o.image, this->real * o.image + this->image * o.real);
+    }
+    Complex & operator += (const Complex &o) {
+        this->real += o.real;
+        this->image += o.image;
+        return *this;
+    }
+};
+
+#define debug(...) cout << " [-] ", _dbg(#__VA_ARGS__, __VA_ARGS__)
+template<class TH> void _dbg(const char *sdbg, TH h){ cout << sdbg << '=' << h << endl; }
+template<class TH, class... TA> void _dbg(const char *sdbg, TH h, TA... a) {
+    while(*sdbg != ',') cout << *sdbg++;
+    cout << '=' << (h) << ','; 
+    _dbg(sdbg+1, a...);
+}
+
+ostream & operator << (ostream &os, Complex x){
+    cout << '{'
+    << x.real << ", "
+    << x.image << '}';
+    return os;
+}
+
+#define SUBCARRIER_NUM 600
+#define MAX_STEP 10
+
+class Complex twiddle_factor[SUBCARRIER_NUM + 1][SUBCARRIER_NUM];
+void precompute_twiddle_factor();
+
+float precomputed_sin[SUBCARRIER_NUM + 1][SUBCARRIER_NUM];
+float precomputed_cos[SUBCARRIER_NUM + 1][SUBCARRIER_NUM];
+void precompute_sincos();
+
+int precomputed_permutation[SUBCARRIER_NUM + 1][SUBCARRIER_NUM];
+void precompute_permutation();
+
+int step_number[SUBCARRIER_NUM + 1];
+int step_radix[SUBCARRIER_NUM + 1][MAX_STEP];
+void precompute_step_number();
+
+int radices[] = {59, 57, 53, 47, 43, 37, 31, 29, 23, 19, 17, 13, 11, 7, 5, 3, 2};
+
+void precompute_all();
+void precomputed_fft(int size, Complex *list);
